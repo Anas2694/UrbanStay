974071ca62b6db661448cfdfb97a98a0ead01b53
Anas <viquaruddinanas2694@gmail.com>
2026-03-15 23:59:45 +0530

    fix homepage route

--- app.js
+++ app.js
@@ -96,6 +96,11 @@ app.use((req, res, next) => {
     next();
 });
 
+
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
+
 app.use("/", userRoutes);
 app.use("/listings", listingRoutes);
 app.use("/listings/:id/reviews", reviewRoutes);
